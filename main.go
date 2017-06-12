d26527aeadab4ee7812dcf3dba6af86b6c28223b
shuchao <645904877@qq.com>
2017-06-12 10:12:57 +0800

    fix makefile format

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "runtime"
+import (
+    "runtime"
+    "github.com/BluePecker/JwtAuth/cmd/jwtauthd"
+)
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())
     
+    if err := jwtauthd.JwtAuth.Cmd.Execute(); err != nil {
+        panic(err)
+    }
 }
