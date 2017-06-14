d1f1b6879340c40d902c0337580a58b5ea5d1dbb
shuchao <645904877@qq.com>
2017-06-14 12:06:29 +0800

    debug

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
     "runtime"
     "github.com/BluePecker/JwtAuth/cmd/jwtauthd"
+    "fmt"
 )
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())
     
     jwtauthd.JwtAuth.Cmd.Execute()
+    
+    fmt.Println(jwtauthd.JwtAuth.Args)
 }
