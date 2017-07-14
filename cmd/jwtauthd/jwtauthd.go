cddfa6a23cae39a0bbfff65dd2c8cefc208106c1
shuchao <645904877@qq.com>
2017-07-14 10:27:13 +0800

    debug

--- cmd/jwtauthd/jwtauthd.go
+++ cmd/jwtauthd/jwtauthd.go
@@ -44,23 +44,52 @@ type JwtAuthCommand struct {
 var JwtAuth *JwtAuthCommand = &JwtAuthCommand{}
 
 func UsageTemplate() string {
-    return `Usage:{{if .Runnable}}
-  {{if .HasAvailableFlags}}{{appendIfNotPresent .UseLine "[OPTIONS] COMMAND [arg...]"}}{{else}}{{.UseLine}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
-  {{ .CommandPath}} [command]{{end}}{{if gt .Aliases 0}}
-Aliases:
-  {{.NameAndAliases}}
-{{end}}{{if .HasExample}}
-Examples:
-{{ .Example }}{{end}}{{ if .HasAvailableSubCommands}}
-Commands:{{range .Commands}}{{if .IsAvailableCommand}}
-  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableLocalFlags}}
-Options:
-{{.LocalFlags.FlagUsages | trimRightSpace}}{{end}}{{ if .HasAvailableInheritedFlags}}
-Global Flags:
-{{.InheritedFlags.FlagUsages | trimRightSpace}}{{end}}{{if .HasHelpSubCommands}}
-Additional help topics:{{range .Commands}}{{if .IsHelpCommand}}
-  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableSubCommands }}
-Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+    return `Usage:
+    {{if .Runnable}}
+        {{if .HasAvailableFlags}}
+            {{appendIfNotPresent .UseLine "[OPTIONS] COMMAND [arg...]"}}
+        {{else}}
+            {{.UseLine}}
+        {{end}}
+    {{end}}
+    {{if .HasAvailableSubCommands}}
+        {{ .CommandPath}} [command]
+    {{end}}
+    {{if gt .Aliases 0}}
+        Aliases:
+        {{.NameAndAliases}}
+    {{end}}
+    {{if .HasExample}}
+        Examples:
+        {{ .Example }}
+    {{end}}
+    {{ if .HasAvailableSubCommands}}
+        Commands:{{range .Commands}}
+        {{if .IsAvailableCommand}}
+            {{rpad .Name .NamePadding }}
+                {{.Short}}
+            {{end}}
+        {{end}}
+    {{end}}
+    {{ if .HasAvailableLocalFlags}}
+        Options:
+        {{.LocalFlags.FlagUsages | trimRightSpace}}
+    {{end}}
+    {{ if .HasAvailableInheritedFlags}}
+        Global Flags:
+        {{.InheritedFlags.FlagUsages | trimRightSpace}}
+    {{end}}
+    {{if .HasHelpSubCommands}}
+        Additional help topics:{{range .Commands}}
+        {{if .IsHelpCommand}}
+            {{rpad .CommandPath .CommandPathPadding}}
+                {{.Short}}
+            {{end}}
+        {{end}}
+    {{end}}
+    {{ if .HasAvailableSubCommands }}
+        Use "{{.CommandPath}} [command] --help" for more information about a command.
+    {{end}}
 `
 }
 
