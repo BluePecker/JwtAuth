fb5abd099766e567baa565c9c5fc8c9299f9150b
shuchao <645904877@qq.com>
2017-09-04 10:59:25 +0800

    fix bug

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -132,7 +132,7 @@ func NewStart(args Options) {
         level, err := logrus.ParseLevel(args.LogLevel)
         if err == nil {
             logrus.SetLevel(level)
-            logrus.SetFormatter(logrus.TextFormatter{
+            logrus.SetFormatter(&logrus.TextFormatter{
                 TimestampFormat: "2006-01-02 15:04:05",
             })
         } else {
