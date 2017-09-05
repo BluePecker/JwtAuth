9521c97fa8550fc436f11ea6697db5fe39651e9b
shuchao <645904877@qq.com>
2017-09-06 01:13:44 +0800

    fix bug

--- server/router/token/routes.go
+++ server/router/token/routes.go
@@ -9,25 +9,29 @@ import (
 func (auth *Router) auth(ctx context.Context) {
     req := &token.AuthRequest{}
     if err := ctx.ReadJSON(req); err != nil {
-        return Response.Failure(ctx, err.Error())
+        Response.Failure(ctx, err.Error())
+        return
     }
     claims, err := auth.backend.Auth(*req)
     if err != nil {
-        return Response.Failure(ctx, err.Error())
+        Response.Failure(ctx, err.Error())
+        return
     }
-    return Response.Success(ctx, claims)
+    Response.Success(ctx, claims)
 }
 
 func (auth *Router) generate(ctx context.Context) {
     req := &token.GenerateRequest{}
     if err := ctx.ReadJSON(req); err != nil {
-        return Response.Failure(ctx, err.Error())
+        Response.Failure(ctx, err.Error())
+        return
     }
     jwt, err := auth.backend.Generate(*req)
     if err != nil {
-        return Response.Failure(ctx, err.Error())
+        Response.Failure(ctx, err.Error())
+        return
     }
-    return Response.Success(ctx, map[string]interface{}{
+    Response.Success(ctx, map[string]interface{}{
         "token": jwt,
     })
 }
