b1891aa4e2dad2b90c5cd85c744394dcb34619cf
shuchao <645904877@qq.com>
2017-09-06 14:21:11 +0800

    modified template

--- cmd/jwtauthd/jwtauthd.go
+++ cmd/jwtauthd/jwtauthd.go
@@ -58,12 +58,12 @@ Aliases:
 {{end}}{{if .HasExample}}
 Examples:
 {{ .Example }}{{end}}{{ if .HasAvailableSubCommands}}
-Commands:{{range .Commands}}{{if .IsAvailableCommand}}
-  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableLocalFlags}}
 Options:
 {{.LocalFlags.FlagUsages | trimRightSpace}}{{end}}{{ if .HasAvailableInheritedFlags}}
 Global Flags:
 {{.InheritedFlags.FlagUsages | trimRightSpace}}{{end}}{{if .HasHelpSubCommands}}
+Commands:{{range .Commands}}{{if .IsAvailableCommand}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableLocalFlags}}
 Additional help topics:{{range .Commands}}{{if .IsHelpCommand}}
   {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableSubCommands }}
 Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
