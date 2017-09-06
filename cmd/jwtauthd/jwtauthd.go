8e6cf5bded52d77871ed82f2af8aa40647de11b0
shuchao <645904877@qq.com>
2017-09-06 14:23:06 +0800

    fix bug

--- cmd/jwtauthd/jwtauthd.go
+++ cmd/jwtauthd/jwtauthd.go
@@ -57,13 +57,13 @@ Aliases:
   {{.NameAndAliases}}
 {{end}}{{if .HasExample}}
 Examples:
-{{ .Example }}{{end}}{{ if .HasAvailableSubCommands}}
+{{ .Example }}{{end}}{{ if .HasAvailableLocalFlags}}
 Options:
-{{.LocalFlags.FlagUsages | trimRightSpace}}{{end}}{{ if .HasAvailableInheritedFlags}}
+{{.LocalFlags.FlagUsages | trimRightSpace}}{{end}}{{ if .HasAvailableSubCommands}}
+Commands:{{range .Commands}}{{if .IsAvailableCommand}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableInheritedFlags}}
 Global Flags:
 {{.InheritedFlags.FlagUsages | trimRightSpace}}{{end}}{{if .HasHelpSubCommands}}
-Commands:{{range .Commands}}{{if .IsAvailableCommand}}
-  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableLocalFlags}}
 Additional help topics:{{range .Commands}}{{if .IsHelpCommand}}
   {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableSubCommands }}
 Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
