198979d58d22d69cea05b29d8b7ee6c119e88359
shuchao <645904877@qq.com>
2017-10-16 15:37:50 +0800

    debug

--- dialog/client/list/list.go
+++ dialog/client/list/list.go
@@ -4,9 +4,9 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/BluePecker/JwtAuth/dialog/client"
 	"fmt"
-	"strings"
 	"encoding/json"
 	"github.com/BluePecker/JwtAuth/dialog/server/parameter/token/request"
+	"bytes"
 )
 
 func NewCommand() *cobra.Command {
@@ -21,7 +21,7 @@ func NewCommand() *cobra.Command {
 			body, _ := json.Marshal(request.List{Unique: args[0]})
 			cli := client.NewClient(unixSock)
 			if body, err := cli.Post("/v1.0/token/list",
-				"application/json", strings.NewReader(string(body))); err != nil {
+				"application/json", bytes.NewBuffer([]byte(body))); err != nil {
 				return err
 			} else {
 				fmt.Printf("result: %s\n", body)
