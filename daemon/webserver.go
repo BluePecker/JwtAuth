c1eb41cbe91012a114296f085b75726d89719ad6
shuchao <645904877@qq.com>
2017-10-16 14:26:28 +0800

    server coder

--- daemon/webserver.go
+++ daemon/webserver.go
@@ -15,7 +15,6 @@ func (d *Daemon) WebServer(ch chan struct{}) error {
 		l, err := netutil.UNIX(d.Options.SockFile, 0666)
 		if err == nil {
 			(&webserver.Web{}).Listen(iris.Listener(l), nil,
-				coder.NewRoute(d),
 				signal.NewRoute(d),
 			)
 		}
@@ -26,6 +25,7 @@ func (d *Daemon) WebServer(ch chan struct{}) error {
 	if d.Options.TLS.Key != "" && d.Options.TLS.Cert != "" {
 		runner = iris.TLS(addr, d.Options.TLS.Cert, d.Options.TLS.Key)
 	}
-	// todo add route
-	return (&webserver.Web{}).Listen(runner, ch)
+	return (&webserver.Web{}).Listen(runner, ch,
+		coder.NewRoute(d),
+	)
 }
