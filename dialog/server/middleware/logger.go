d6a96e9686c252c1922aca251b86543a3bec0459
shuchao <645904877@qq.com>
2017-10-23 23:36:31 +0800

    remove test info

--- dialog/server/middleware/logger.go
+++ dialog/server/middleware/logger.go
@@ -10,7 +10,6 @@ import (
 func init() {
 	Register(func(ctx context.Context) {
 		start := time.Now()
-		logrus.Info("test")
 		ctx.Next()
 		logrus.Infof("%v %4v %s %s %s", strconv.Itoa(ctx.GetStatusCode()), time.Now().Sub(start), ctx.RemoteAddr(), ctx.Method(), ctx.Path())
 	})
