f71f048554bd8a905f7707ccf0f4213646d70dee
shuchao <645904877@qq.com>
2017-10-23 23:34:50 +0800

    debug

--- dialog/server/server.go
+++ dialog/server/server.go
@@ -9,18 +9,22 @@ import (
 type (
 	WebServer struct {
 		Engine *iris.Application
+		routes []router.Route
 	}
 )
 
 func (w *WebServer) AddRoute(routes ... router.Route) {
 	for _, route := range routes {
-		route.Routes(w.Engine)
+		w.routes = append(w.routes, route)
 	}
 }
 
 func (w *WebServer) Run(runner iris.Runner, configurator ... iris.Configurator) error {
 	for _, ware := range middleware.Provider {
 		w.Engine.Use(ware)
+	}
+	for _, route := range w.routes {
+		route.Routes(w.Engine)
 	}
 	return w.Engine.Run(runner, configurator...)
 }
